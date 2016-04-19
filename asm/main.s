c5243a94d1a48d564fdc124f2f64af1b29083992
boblaublaw <github@sublimation.org>
2016-04-19 13:07:25 -0700

    initial checkin

---
+++ asm/main.s
@@ -0,0 +1,29 @@
+;
+;  main.s
+;  test_asm
+;
+;  Created by joe on 4/17/16.
+;  Copyright © 2016 joe. All rights reserved.
+;
+
+COUT = $fded
+KEYBOARD = $c000
+STROBE = $c010
+
+main:
+    ldx #$00
+loop:
+    lda msg, X
+    beq wait
+    ora #$80
+    jsr COUT
+    inx
+    jmp loop
+    
+wait:
+    lda KEYBOARD
+    bpl wait
+    lda STROBE
+    rts
+
+msg:    .asciiz "HELLO ALSO, ALISON"
---
+++ c/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ * test_C_program
+ *
+ * Created by joe on 4/17/16.
+ * Copyright (c) 2016 joe. All rights reserved.
+ *
+ */
+
+
+#include <stdio.h>
+#include <conio.h>
+
+
+int main(void)
+{
+    printf("HELLO ALISON!\n");
+    cgetc();
+    return 0;
+}
